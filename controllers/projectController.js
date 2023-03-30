5e576566da4baae9c25c21514eccafb506cf1564
ahmed <ahmed180107@feng.bu.edu.eg>
2023-03-30 15:21:42 +0200

    View_All_Projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -14,3 +14,18 @@ exports.createProject = async (req, res) => {
     });
   }
 };
+exports.getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(201).json({
+      status: "success",
+      results: projects.length,
+      data: projects,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      error: err.message,
+    });
+  }
+};
