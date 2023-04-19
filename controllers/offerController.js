60a24ed7254b4323161fe4d584e4c45238de484a
ahmed <ahmed180107@feng.bu.edu.eg>
2023-04-19 23:41:39 +0200

    GetOffer&Filter

--- controllers/offerController.js
+++ controllers/offerController.js
@@ -3,17 +3,45 @@ const Offer = require("../models/offerModel");
 exports.getAllOffers = async (req, res) => {
   try {
     const offers = await Offer.find();
-    res.status(200).json({
+    const filters = req.query;
+    const filteredOffers = offers.filter((offer) => {
+      let isValid = true;
+      for (key in filters) {
+        isValid = isValid && offer[key] == filters[key];
+      }
+      return isValid;
+    });
+    res.status(201).json({
       status: "success",
-      results: offers.length,
-      data: {
-        offers,
-      },
+      length: filteredOffers.length,
+      data: filteredOffers,
     });
   } catch (err) {
     res.status(404).json({
       status: "fail",
       message: err,
+    });
+  }
+};
+exports.getOneOffer = async (req, res) => {
+  try {
+    const offer = await Offer.findById(req.params.id);
+    if (!offer) {
+      return res.status(404).json({
+        status: "fail",
+        message: "ofer not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        offer,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
     });
   }
 };
@@ -82,10 +110,14 @@ exports.deleteOffer = async (req, res) => {
 };
 exports.acceptOffer = async (req, res) => {
   try {
-    const offer = await Offer.findByIdAndUpdate(req.params.id, {status: 'accepted'}, {
-      new: true,
-      runValidators: true,
-    });
+    const offer = await Offer.findByIdAndUpdate(
+      req.params.id,
+      { status: "accepted" },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
     if (!offer) {
       return res.status(404).json({
         status: "fail",
@@ -108,10 +140,14 @@ exports.acceptOffer = async (req, res) => {
 
 exports.rejectOffer = async (req, res) => {
   try {
-    const offer = await Offer.findByIdAndUpdate(req.params.id, {status: 'rejected'}, {
-      new: true,
-      runValidators: true,
-    });
+    const offer = await Offer.findByIdAndUpdate(
+      req.params.id,
+      { status: "rejected" },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
     if (!offer) {
       return res.status(404).json({
         status: "fail",
@@ -131,6 +167,3 @@ exports.rejectOffer = async (req, res) => {
     });
   }
 };
-
-
-
